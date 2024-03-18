94f866633e547fa05f287fcd21ca5ab0d3ade5dc
Rob Godfrey <rob_godfrey@outlook.com>
2024-03-18 00:40:46 -1000

    Add jump and temp idle anim, flip left/right

--- Assets/Scripts/RbMove2D.cs
+++ Assets/Scripts/RbMove2D.cs
@@ -9,30 +9,44 @@ public class RbMove2D : MonoBehaviour
     public int jumpSpeed = 10;
     public float dampConstant = 0.75f;
     public Vector3 movement;
+
     private float upVel = 1;
     private float impactVelocity;
     private int jumpCount = 0;
     private bool isJumping = false;
+    private float horizontal;
+    private float vertical;
+    private bool facingRight = true;
     private Rigidbody rb;
+
+    [SerializeField] private Animator anim;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        movement = new Vector3(horizontal, 0, vertical).normalized;
         if (jumpCount < allowableJumps) {
             if (Input.GetKeyDown(KeyCode.Space)) {
                 upVel = jumpSpeed;
                 isJumping = true;
+                anim.SetTrigger("Jump");
             }
         }
+
+        Flip();
+
+        anim.SetBool("Moving", Mathf.Abs(horizontal + vertical) > 0);
     }
 
-    void FixedUpdate() {
+    private void FixedUpdate() {
         // add current y velocity instead of setting to 0 every time
         rb.velocity = movement * speed * Time.fixedDeltaTime + new Vector3(0, rb.velocity.y, 0);
         impactVelocity = rb.velocity.y;
@@ -43,7 +57,7 @@ public class RbMove2D : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter(Collision hit) {
+    private void OnCollisionEnter(Collision hit) {
         jumpCount = allowableJumps;
         upVel = -impactVelocity * Mathf.Sqrt(dampConstant);
         isJumping = true;
@@ -51,8 +65,19 @@ public class RbMove2D : MonoBehaviour
         jumpCount = 0;
     }
 
-    void OnCollisionExit(Collision hit) {
+    private void OnCollisionExit(Collision hit) {
         // prevents midair jump
         jumpCount++;
+    }
+
+    private void Flip()
+    {
+        if (facingRight && horizontal < 0 || !facingRight && horizontal > 0)
+        {
+            facingRight = !facingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
